2522407766978756c4240cc9bbca0ef399b8a8b4
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-09-12 15:29:58 -0300

    add calculation in second column

--- src/views/SimpleBakerCalculator.vue
+++ src/views/SimpleBakerCalculator.vue
@@ -1,115 +1,206 @@
 <template>
-  <v-card width="600px" class="mt-10 mx-auto">
-    <v-card-title primary-title>
-      <h2>Calculadora</h2>
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field
-          label="Peso Total de Masa"
-          v-model="totalDoughWeight"
-          suffix="grs"
-        ></v-text-field>
-        <v-text-field
-          label="% Masa madre al 100% hidratación"
-          v-model="sourdough"
-          suffix="%"
-        ></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="sourdoughFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-        <v-divider></v-divider>
-        <v-text-field
-          label="Tipo Harina"
-          v-model="fluors.one.type"
-        ></v-text-field>
-        <v-text-field
-          :label="flourTypeOne"
-          v-model="fluors.one.percent"
-          suffix="%"
-        ></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="fluorOneFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-        <p v-if="totalFluorPercentGreaterThan100">
-          Total de harinas no debe superar el 100%
-        </p>
-        <v-divider></v-divider>
-        <v-text-field
-          label="Tipo Harina"
-          v-model="fluors.two.type"
-        ></v-text-field>
-        <v-text-field
-          :label="flourTypeTwo"
-          v-model="fluors.two.percent"
-          suffix="%"
-        ></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="fluorTwoFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-        <p v-if="totalFluorPercentGreaterThan100">
-          Total de harinas no debe superar el 100%
-        </p>
-        <v-divider></v-divider>
-        <v-text-field
-          label="Tipo Harina"
-          v-model="fluors.three.type"
-        ></v-text-field>
-        <v-text-field
-          :label="flourTypeThree"
-          v-model="fluors.three.percent"
-          suffix="%"
-        ></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="fluorThreeFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-        <p v-if="totalFluorPercentGreaterThan100">
-          Total de harinas no debe superar el 100%
-        </p>
-        <v-divider></v-divider>
-        <v-text-field label="Agua" v-model="water" suffix="%"></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="waterFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-        <v-divider></v-divider>
-        <v-text-field label="Sal" v-model="salt" suffix="%"></v-text-field>
-        <v-text-field
-          label="Total in grams"
-          :value="saltFromTotalDoughWeight"
-          outlined
-          readonly
-          suffix="grs"
-        ></v-text-field>
-      </v-form>
-    </v-card-text>
-  </v-card>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <v-card class="mt-10 mx-auto">
+            <v-card-title primary-title>
+              <h3>Calculadora</h3>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <v-text-field
+                label="Peso Total de Harina"
+                v-model="totalFluorWeight"
+                suffix="grs"
+              ></v-text-field>
+              <v-text-field
+                label="% Masa madre al 100% hidratación"
+                v-model="sourdough"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="sourdoughFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <v-divider></v-divider>
+              <v-text-field
+                label="Agua"
+                v-model="water"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="waterFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <v-divider></v-divider>
+              <v-text-field
+                label="Sal"
+                v-model="salt"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="saltFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <v-divider></v-divider>
+              <v-text-field
+                label="Tipo Harina"
+                v-model="fluors.one.type"
+              ></v-text-field>
+              <v-text-field
+                :label="flourTypeOne"
+                v-model="fluors.one.percent"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="fluorOneFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <p v-if="totalFluorPercentGreaterThan100">
+                Total de harinas no debe superar el 100%
+              </p>
+              <v-divider></v-divider>
+              <v-text-field
+                label="Tipo Harina"
+                v-model="fluors.two.type"
+              ></v-text-field>
+              <v-text-field
+                :label="flourTypeTwo"
+                v-model="fluors.two.percent"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="fluorTwoFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <p v-if="totalFluorPercentGreaterThan100">
+                Total de harinas no debe superar el 100%
+              </p>
+              <v-divider></v-divider>
+              <v-text-field
+                label="Tipo Harina"
+                v-model="fluors.three.type"
+              ></v-text-field>
+              <v-text-field
+                :label="flourTypeThree"
+                v-model="fluors.three.percent"
+                suffix="%"
+              ></v-text-field>
+              <v-text-field
+                label="Total in grams"
+                :value="fluorThreeFromTotalFluorWeight"
+                outlined
+                readonly
+                suffix="grs"
+              ></v-text-field>
+              <p v-if="totalFluorPercentGreaterThan100">
+                Total de harinas no debe superar el 100%
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card class="mt-10 mx-auto">
+            <v-card-title primary-title>
+              <h3>Resultado</h3>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <v-list disabled>
+                <v-list-item-group color="primary">
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        Total de masa madre
+                        {{ sourdoughFromTotalFluorWeight }} grs
+                      </v-list-item-title>
+                      <v-list-item-subtitle
+                        >Agua
+                        {{ sourdoughFromTotalFluorWeight / 2 }}
+                        grs</v-list-item-subtitle
+                      >
+                      <v-list-item-subtitle
+                        >Harina
+                        {{ sourdoughFromTotalFluorWeight / 2 }}
+                        grs</v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        Total de agua {{ waterFromTotalFluorWeight }} grs
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        Total de sal {{ saltFromTotalFluorWeight }} grs
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        >Total de Harinas
+                        {{ totalFluorWeight || 0 }} grs</v-list-item-title
+                      >
+                      <v-list-item-subtitle v-if="fluors.one.type"
+                        >{{ fluors.one.type }} -
+                        {{ fluorOneFromTotalFluorWeight }}
+                        grs</v-list-item-subtitle
+                      >
+                      <v-list-item-subtitle v-if="fluors.two.type"
+                        >{{ fluors.two.type }} -
+                        {{ fluorTwoFromTotalFluorWeight }}
+                        grs</v-list-item-subtitle
+                      >
+                      <v-list-item-subtitle v-if="fluors.three.type"
+                        >{{ fluors.three.type }} -
+                        {{ fluorThreeFromTotalFluorWeight }}
+                        grs</v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        Total de la masa {{ totalDoughWeight }} grs
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      totalDoughWeight: "",
+      totalFluorWeight: "",
       sourdough: "",
       fluors: {
         one: {
@@ -143,29 +234,29 @@ export default {
     flourTypeThree: function() {
       return "% Harina " + this.fluors.three.type;
     },
-    sourdoughFromTotalDoughWeight: function() {
-      return (Number(this.sourdough) * this.totalDoughWeight) / 100 || 0;
+    sourdoughFromTotalFluorWeight: function() {
+      return (Number(this.sourdough) * this.totalFluorWeight) / 100 || 0;
     },
-    fluorOneFromTotalDoughWeight: function() {
+    fluorOneFromTotalFluorWeight: function() {
       return (
-        (Number(this.fluors.one.percent) * this.totalDoughWeight) / 100 || 0
+        (Number(this.fluors.one.percent) * this.totalFluorWeight) / 100 || 0
       );
     },
-    fluorTwoFromTotalDoughWeight: function() {
+    fluorTwoFromTotalFluorWeight: function() {
       return (
-        (Number(this.fluors.two.percent) * this.totalDoughWeight) / 100 || 0
+        (Number(this.fluors.two.percent) * this.totalFluorWeight) / 100 || 0
       );
     },
-    fluorThreeFromTotalDoughWeight: function() {
+    fluorThreeFromTotalFluorWeight: function() {
       return (
-        (Number(this.fluors.three.percent) * this.totalDoughWeight) / 100 || 0
+        (Number(this.fluors.three.percent) * this.totalFluorWeight) / 100 || 0
       );
     },
-    waterFromTotalDoughWeight: function() {
-      return (Number(this.water) * this.totalDoughWeight) / 100 || 0;
+    waterFromTotalFluorWeight: function() {
+      return (Number(this.water) * this.totalFluorWeight) / 100 || 0;
     },
-    saltFromTotalDoughWeight: function() {
-      return (Number(this.salt) * this.totalDoughWeight) / 100 || 0;
+    saltFromTotalFluorWeight: function() {
+      return (Number(this.salt) * this.totalFluorWeight) / 100 || 0;
     },
     totalFluorPercentGreaterThan100: function() {
       return (
@@ -173,6 +264,14 @@ export default {
           Number(this.fluors.two.percent) +
           Number(this.fluors.three.percent) >
         100
+      );
+    },
+    totalDoughWeight: function() {
+      return (
+        Number(this.totalFluorWeight) +
+        Number(this.waterFromTotalFluorWeight) +
+        Number(this.saltFromTotalFluorWeight) +
+        Number(this.sourdoughFromTotalFluorWeight)
       );
     }
   }
